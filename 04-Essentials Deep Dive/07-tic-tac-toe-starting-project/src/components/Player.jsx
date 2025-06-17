d9fada70765d441b10f6d1fd27a7c30464bb0f40
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-06-17 23:32:02 +0500

    React Essentials Deep Dive

--- 04-Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 04-Essentials Deep Dive/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
-export default function Player({ initialName, symbol, isActive }) {
-
+export default function Player({
+  initialName,
+  symbol,
+  isActive,
+  onChangeName,
+}) {
   const [isEditing, setIsEditing] = useState(false);
-  const [playerName, setPlayerName] = useState(initialName)
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
-    // setIsEditing(isEditing ? false : true);
     setIsEditing((editing) => !editing);
+
+    if (isEditing) {
+      onChangeName(symbol, playerName);
+    }
   }
 
   function handleChange(event) {
@@ -15,15 +22,15 @@ export default function Player({ initialName, symbol, isActive }) {
   }
 
   let editablePlayerName = <span className="player-name">{playerName}</span>;
-  // let btnCaption = 'Edit';
 
   if (isEditing) {
-    editablePlayerName = <input type="text" required value={playerName} onChange={handleChange} />;
-    // btnCaption="Save"
+    editablePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
   }
 
   return (
-    <li className={isActive ? 'active' : undefined} >
+    <li className={isActive ? "active" : undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
