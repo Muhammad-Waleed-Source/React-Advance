da3fa92b4633cfad955906d2ca6444e4d3b714f8
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-08-05 22:15:50 +0500

    Advanced Redux Completed

--- 20-Advanced Redux/advance-redux/src/components/Shop/ProductItem.jsx
+++ 20-Advanced Redux/advance-redux/src/components/Shop/ProductItem.jsx
@@ -1,20 +1,25 @@
-import { cartActions } from '../../store/cart-slice';
-import Card from '../UI/Card';
-import classes from './ProductItem.module.css';
-import {useDispatch} from 'react-redux'
-
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../store/cart-slice";
+import Card from "../UI/Card";
+import classes from "./ProductItem.module.css";
 
 const ProductItem = (props) => {
-  const { title, price, description, id } = props;
   const dispatch = useDispatch();
 
-  function addToCartHandler() {
-    dispatch(cartActions.addItemToCart({
-      id: id,
-      title,
-      price
-    }));
-  }
+  const { title, price, description, id } = props;
+
+  const addToCartHandler = () => {
+    // and then send Http request
+    // fetch('firebase-url', { method: 'POST', body: JSON.stringify(newCart) })
+
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
 
   return (
     <li className={classes.item}>
--- 20-Advanced Redux/advance-redux/src/store/cart-slice.js
+++ 20-Advanced Redux/advance-redux/src/store/cart-slice.js
@@ -5,13 +5,19 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+      state.changed = false; // Data just loaded from DB — not user edit
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -22,16 +28,16 @@ const cartSlice = createSlice({
         });
       } else {
         existingItem.quantity++;
-        // existingItem.totalPrice = totalPrice + newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
-
+      state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
-        state.totalQuantity--;
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
