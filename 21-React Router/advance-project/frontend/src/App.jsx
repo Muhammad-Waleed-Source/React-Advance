edb7bfe375a45d37b462f6bf3d546a125a206626
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-08-08 23:08:14 +0500

    08/08/2025

--- 21-React Router/advance-project/frontend/src/App.jsx
+++ 21-React Router/advance-project/frontend/src/App.jsx
@@ -3,11 +3,12 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/Root";
 import Home from "./pages/Home";
-import Events from "./pages/Events";
+import Events, { loader as eventsLoader } from "./pages/Events";
 import EventDetail from "./pages/EventDetail";
 import NewEvent from "./pages/NewEvent";
 import EditEvent from "./pages/EditEvent";
 import EventsRoot from "./pages/EventsRoot";
+import Error from "./pages/Error";
 
 // 1. Add five new (dummy) page components (content can be simple <h1> elements)
 //    - HomePage
@@ -33,13 +34,18 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <Error />,
     children: [
       { index: true, element: <Home /> },
       {
         path: "events",
         element: <EventsRoot />,
         children: [
-          { index: true, element: <Events /> },
+          {
+            index: true,
+            element: <Events />,
+            loader: eventsLoader,
+          },
           { path: ":eventId", element: <EventDetail /> },
           { path: "new", element: <NewEvent /> },
           { path: ":eventId/edit", element: <EditEvent /> },
--- 21-React Router/advance-project/frontend/src/pages/Events.jsx
+++ 21-React Router/advance-project/frontend/src/pages/Events.jsx
@@ -1,22 +1,30 @@
-import { Link } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
-const EVENTS = [
-  { id: "e1", title: "Event 1" },
-  { id: "e2", title: "Event 2" },
-  { id: "e3", title: "Event 3" },
-];
+function EventsPage() {
+  const data = useLoaderData();
 
-export default function Events() {
-  return (
-    <>
-      <h1>Events Page</h1>
-      <ul>
-        {EVENTS.map((event) => (
-            <li key={event.id}>
-                <Link to={`/events/${event.id}`}>{event.title}</Link>
-            </li>
-        ))}
-      </ul>
-    </>
-  );
+  // if(data.isError){
+  //   return <p>{data.message}</p>
+  // }
+  const events = data.events;
+
+  return <EventsList events={events} />;
+}
+
+export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    // return {isError: true, message: 'Could not fetch events.'}
+    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+      status: 500,
+    });
+  } else {
+    // const resData = await response.json();
+    // return resData.events;
+    return response;
+  }
 }
